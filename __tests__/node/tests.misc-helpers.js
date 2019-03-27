ff86cefaa116334295265546618cb7c28c344048
te <te.artisin@gmail.com>
2019-03-27 12:24:29 -0500

    fix(lib): fix isAction - alway return boolean → #4
    CLOSES #4

--- __tests__/node/tests.misc-helpers.js
+++ __tests__/node/tests.misc-helpers.js
@@ -3,6 +3,7 @@ const {
   assert,
 } = require('chai');
 const {
+  isAction,
   isIterable,
 } = require(global.DIST_PATH);
 
@@ -16,35 +17,39 @@ const generator = _.attempt(function () {
 
 
 describe('Misc Helpers', function () {
-  it('isIterable() fn 🠲 should return `true` for empty string', function () {
-    assert.strictEqual(isIterable(''), true);
-    // assert.strictEqual(isIterable(String()), false);
-    // assert.strictEqual(isIterable(String('')), false);
-    // assert.strictEqual(isIterable(String('a')), true);
-    // assert.strictEqual(isIterable('ab'), true);
-    // assert.strictEqual(isIterable(generator(2)), true);
-    // assert.strictEqual(isIterable(new Uint8Array([0x00, 0xff])), true);
-    // assert.strictEqual(isIterable(new Map([['a', 1], ['b', 2], ['c', 3]])), true);
-    // assert.strictEqual(isIterable(new Set([1, 1, 2, 2, 3, 3])), true);
-    // (function () {
-    //   assert.strictEqual(isIterable(arguments), true);
-    // })(1, 2, 3);
-    // assert.strictEqual(isIterable({
-    //   0: 1,
-    //   length: 1,
-    // }), false);
-    // assert.strictEqual(isIterable(Object), false);
-    // assert.strictEqual(isIterable({}), false);
-    // assert.strictEqual(isIterable(1), false);
-    // assert.strictEqual(isIterable(0), false);
-    // assert.strictEqual(isIterable(NaN), false);
-    // assert.strictEqual(isIterable(), false);
-    // assert.strictEqual(isIterable(undefined), false);
-    // assert.strictEqual(isIterable(false), false);
-    // assert.strictEqual(isIterable(true), false);
-    // assert.strictEqual(isIterable(Array.isArray), false);
-    // assert.strictEqual(isIterable(/x/), false);
-    // assert.strictEqual(isIterable(Symbol('foo')), false);
+  it('isIterable() fn', function () {
+    assert.strictEqual(isIterable('abc'), true);
+    assert.strictEqual(isIterable(new Map([[1, 'a'], [2, 'b'], [3, 'c']])), true);
+    const myObj = {};
+    assert.strictEqual(isIterable(new WeakMap([[{}, 'a'], [myObj, 'b'], [{}, 'c']]).get(myObj)), true);
+    assert.strictEqual(isIterable([...'abc']), true);
+    assert.strictEqual(isIterable(generator(2)), true);
+    assert.strictEqual(isIterable(new Uint8Array([0x00, 0xff])), true);
+    (function () {
+      assert.strictEqual(isIterable(arguments), true);
+    })(1, 2, 3);
+    assert.strictEqual(isIterable({
+      0: 1,
+      length: 1,
+    }), false);
+    assert.strictEqual(isIterable(Object), false);
+    assert.strictEqual(isIterable({}), false);
+    assert.strictEqual(isIterable(1), false);
+    assert.strictEqual(isIterable(0), false);
+    assert.strictEqual(isIterable(NaN), false);
+    assert.strictEqual(isIterable(), false);
+    assert.strictEqual(isIterable(undefined), false);
+    assert.strictEqual(isIterable(false), false);
+    assert.strictEqual(isIterable(true), false);
+    assert.strictEqual(isIterable(Array.isArray), false);
+    assert.strictEqual(isIterable(/x/), false);
+    assert.strictEqual(isIterable(Symbol('foo')), false);
+  });
+
+  it('isAction() fn', function () {
+    assert.strictEqual(isAction({type: 'ACTION'}), true);
+    assert.strictEqual(isAction({type: 'ACTION', payload: true}), true);
+    assert.strictEqual(isAction({payload: true}), false);
   });
 });
 
